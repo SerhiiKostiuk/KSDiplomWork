3e9c284e7ea893ccecebb5b3a97de4494be907ae
Serhii Kostiuk <k.seryoga@gmail.com>
2016-05-11 11:48:49 +0300

    add new project

---
+++ KSPersonalFinance/KSPersonalFinance/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KSPersonalFinance
+//
+//  Created by Serg Bla on 11.05.16.
+//  Copyright © 2016 Serg Kostiuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KSPersonalFinance/KSPersonalFinance/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KSPersonalFinance
+//
+//  Created by Serg Bla on 11.05.16.
+//  Copyright © 2016 Serg Kostiuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
