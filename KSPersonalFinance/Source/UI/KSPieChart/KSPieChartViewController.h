489f0f32a176dab66bedc9a92b4d4c78b7a0d47e
Serhii Kostiuk <k.seryoga@gmail.com>
2016-05-13 20:54:50 +0300

    add property for pieChartView

---
+++ KSPersonalFinance/Source/UI/KSPieChart/KSPieChartViewController.h
@@ -0,0 +1,16 @@
+//
+//  KSPieChartViewController.h
+//  KSPersonalFinance
+//
+//  Created by Serg Bla on 12.05.16.
+//  Copyright © 2016 Serg Kostiuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MCPieChartView.h"
+
+@interface KSPieChartViewController : UIViewController
+@property (weak, nonatomic) IBOutlet MCPieChartView *pieChartView;
+
+@end
