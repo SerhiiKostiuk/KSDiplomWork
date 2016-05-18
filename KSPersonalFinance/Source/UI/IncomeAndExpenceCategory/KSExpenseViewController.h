83ab1783a72a9de7c4cb679a80cb9b4f9c26a20d
Serhii Kostiuk <k.seryoga@gmail.com>
2016-05-18 23:19:21 +0300

    move some method from numberViewController and refactor them

--- KSPersonalFinance/Source/UI/IncomeAndExpenceCategory/KSExpenseViewController.h
+++ KSPersonalFinance/Source/UI/IncomeAndExpenceCategory/KSExpenseViewController.h
@@ -8,11 +8,21 @@
 
 #import <UIKit/UIKit.h>
 
-@interface KSExpenseViewController : UICollectionViewController
-<
-UICollectionViewDataSource,
-UICollectionViewDelegate,
-UICollectionViewDelegateFlowLayout
->
+typedef NS_ENUM(NSInteger, TransactionType) {
+    TransactionTypeExpense,
+    TransactionTypeIncome
+};
+
+@protocol CategorySelectionDelegate <NSObject>
+
+-(void)selectedCategory:(id)category;
+
+@end
+
+@interface KSExpenseViewController : UIViewController <UICollectionViewDataSource, UICollectionViewDelegate>
+@property (nonatomic, weak) IBOutlet UICollectionView *collectionView;
+@property (nonatomic, weak) id <CategorySelectionDelegate> delegate;
+@property (nonatomic) TransactionType categoryType;
+
 
 @end
