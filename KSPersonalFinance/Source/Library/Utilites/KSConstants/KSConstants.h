7cc040701201110fad4db789d999d426241fe5fb
Serhii Kostiuk <k.seryoga@gmail.com>
2016-05-31 11:14:33 +0300

    add constant header

---
+++ KSPersonalFinance/Source/Library/Utilites/KSConstants/KSConstants.h
@@ -0,0 +1,13 @@
+//
+//  KSConstants.h
+//  KSPersonalFinance
+//
+//  Created by Сергій Костюк on 31.05.16.
+//  Copyright © 2016 Serg Kostiuk. All rights reserved.
+//
+
+#import "KSMacro.h"
+
+KSConstString(kKSExpenseCategoriesFileName, @"expense");
+KSConstString(kKSIncomeCategoriesFileName, @"income");
+KSConstString(fileType, @"plist");
